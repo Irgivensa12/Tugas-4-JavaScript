b6fc537a64d65de72d42de2b67f7a4ae65f5867e
Irgivensa12 <159887639+Irgivensa12@users.noreply.github.com>
2025-03-07 10:34:27 +0700

    first commit

---
+++ logic.js
@@ -0,0 +1,13 @@
+function inputNamNum(){
+let nama = document.getElementById("nama").value;
+
+if(nama.trim == ""){ // trim menghapus spasi di awal & akhir agar dianggap tidak kosong
+    alert("Silahkan Masukkan Nama!");
+    return;
+}
+
+if(angka == ""){
+    alert("Silahkan Masukkan Angka!");
+    return;
+}
+}
