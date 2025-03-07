92cfca72b2c8fce35b5e4dd3c4bcac3083411e62
Irgivensa12 <159887639+Irgivensa12@users.noreply.github.com>
2025-03-07 11:04:20 +0700

    Menambah function untuk menerima input nama dan jml pilihan

--- logic.js
+++ logic.js
@@ -1,13 +1,16 @@
 function inputNamNum(){
 let nama = document.getElementById("nama").value;
+let angka = document.getElementById("angka").value;
 
-if(nama.trim == ""){ // trim menghapus spasi di awal & akhir agar dianggap tidak kosong
+if(nama.trim() == ""){ // trim menghapus spasi di awal & akhir agar dianggap tidak kosong
     alert("Silahkan Masukkan Nama!");
     return;
 }
 
 if(angka == ""){
-    alert("Silahkan Masukkan Angka!");
+    alert("Silahkan Masukkan Pilihan Anda!");
     return;
 }
+console.log("Nama: ", nama);
+console.log("Angka: ", angka)
 }
